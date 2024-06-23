4bbd9ddced31e73e5edd33f85d8d5dc5a256bdb4
Linmaas <linda.j.maas@gmail.com>
2024-06-23 12:47:21 +0200

    added game locgic

---
+++ code/src/ofConfig.h
@@ -0,0 +1,12 @@
+//
+//  ofConfig.h
+//  code
+//
+//  Created by Linda Maas on 23.06.24.
+//
+
+#ifndef ofConfig_h
+#define ofConfig_h
+
+
+#endif /* ofConfig_h */
---
+++ code/src/ofConfig.hpp
@@ -0,0 +1,13 @@
+//
+//  ofConfig.hpp
+//  code
+//
+//  Created by Linda Maas on 23.06.24.
+//
+
+#ifndef ofConfig_hpp
+#define ofConfig_hpp
+
+#include <stdio.h>
+
+#endif /* ofConfig_hpp */
